07cc272e53751947800027dd789764bc00271fed
Constantin Vashchenko <KVashchenko@inno.tech>
2024-07-16 14:17:26 +0300

    add main.go and divided tests

--- main_test.go
+++ main_test.go
@@ -4,48 +4,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 )
-
-var cafeList = map[string][]string{
-	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
@@ -56,17 +17,36 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler.ServeHTTP(responseRecorder, req)
 
 	// здесь нужно добавить необходимые проверки
-
-	// Код 200
-	assert.Equal(t, http.StatusOK, responseRecorder.Code, "Ожидался код 200")
-	// Проверка на пустой ответ в теле
-	assert.NotEmpty(t, responseRecorder.Body.String(), "Тело ответа пришло пустым")
 	// Проверка кол-ва count
 	cafes := strings.Split(responseRecorder.Body.String(), ",")
 	assert.Len(t, cafes, totalCount, "Не соответсвует кол-ву")
 	// Возрашаем все доступные кафе из полученных
 	expectedCafes := strings.Join(cafeList["moscow"], ",")
 	assert.Equal(t, expectedCafes, responseRecorder.Body.String(), "Кафе не соответсвуют ожидаемым кафе")
+}
+
+func TestMainHandlerWhenStatusOkAndBodyNotEmpty(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=7&city=moscow", nil) // здесь нужно создать запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// здесь нужно добавить необходимые проверки
+	// Проверяем код 200
+	assert.Equal(t, http.StatusOK, responseRecorder.Code, "Ожидался код 200")
+	// Проверка на пустой ответ в теле
+	assert.NotEmpty(t, responseRecorder.Body.String(), "Тело ответа пришло пустым")
+}
+
+func TestMainHandlerWhenStatusBadRequestAndWrongErrorMessage(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=7&city", nil) // здесь нужно создать запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// здесь нужно добавить необходимые проверки
 	// Проверяем код 400
 	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code, "Ожидался код 400")
 	// Проверка сообщения об ошибке
